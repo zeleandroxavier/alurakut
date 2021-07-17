e6fdc351a474e14a8e368340104c104da4da5d80
zeleandroxavier <zeleandroxavier@gmail.com>
2021-07-17 15:56:19 -0300

    Aula 3 Finalizando

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet} from '../src/components/lib/AlurakutCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/components/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
-
 function ProfileSidebar(propriedades) {
-  console.log(propriedades);
   return (
     <Box as="aside">
       <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />
@@ -24,12 +22,30 @@ function ProfileSidebar(propriedades) {
   )
 }
 
-
+function ProfileRelationsBox(propriedades) {
+  return (
+    <ProfileRelationsBoxWrapper>
+      <h2 className="smallTitle">
+        {propriedades.title} ({propriedades.items.length})
+      </h2>
+      <ul>
+        {/* {seguidores.map((itemAtual) => {
+          return (
+            <li key={itemAtual}>
+              <a href={`https://github.com/${itemAtual}.png`}>
+                <img src={itemAtual.image} />
+                <span>{itemAtual.title}</span>
+              </a>
+            </li>
+          )
+        })} */}
+      </ul>
+    </ProfileRelationsBoxWrapper>
+  )
+}
 
 export default function Home() {
-
   const usuarioAleatorio = 'zeleandroxavier';
-
   const [comunidades, setComunidades] = React.useState([{
     id: '12802378123789378912789789123896123', 
     title: 'Eu odeio acordar cedo',
@@ -37,8 +53,7 @@ export default function Home() {
   }]);
   // const comunidades = comunidades[0];
   // const alteradorDeComunidades/setComunidades = comunidades[1];
-
-  console.log('Nosso teste', );
+  // const comunidades = ['Alurakut'];
 
   const pessoasFavoritas = [
     'thiagoabreu72',
@@ -51,64 +66,81 @@ export default function Home() {
     'felipefialho',
   ]
 
+  const [seguidores, setSeguidores] = React.useState([]);
+  // 0 - Pegar o array de dados do github 
+  React.useEffect(function() {
+    fetch('https://api.github.com/users/zeleandroxavier/followers')
+    .then(function (respostaDoServidor) {
+      return respostaDoServidor.json();
+    })
+    .then(function(respostaCompleta) {
+      setSeguidores(respostaCompleta);
+    })
+  }, [])
+
+  console.log('seguidores antes do return', seguidores);
+
+  // 1 - Criar um box que vai ter um map, baseado nos items do array
+  // que pegamos do GitHub
+
   return (
     <>
       <AlurakutMenu />
       <MainGrid>
+        {/* <Box style="grid-area: profileArea;"> */}
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={usuarioAleatorio} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">
+              Bem vindo(a) 
+            </h1>
 
-          <div className="profileArea" style={{gridArea: 'profileArea'}}> 
-          <ProfileSidebar githubUser={usuarioAleatorio}/>
-          </div>
+            <OrkutNostalgicIconSet />
+          </Box>
 
-          <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}> 
-            <Box> 
-              <h1 className="title">
-                Bem Vindo
-              </h1>
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form onSubmit={function handleCriaComunidade(e) {
+                e.preventDefault();
+                const dadosDoForm = new FormData(e.target);
 
-              <OrkutNostalgicIconSet />
-          
-            </Box>
-              <Box>
-                
-                <h2 className="subTitle" >O que você deseja fazer?</h2>
-                  <form onSubmit={function handleCriaComunidade(e){
-                     e.preventDefault();/*Função para não recarregar a pagina*/
-                     const dadosDoForm = new FormData(e.target);
+                console.log('Campo: ', dadosDoForm.get('title'));
+                console.log('Campo: ', dadosDoForm.get('image'));
 
-                     console.log('Campo: ', dadosDoForm.get('title'));
-                     console.log('Campo: ', dadosDoForm.get('image'));
-     
-                     const comunidade = {
-                       id: new Date().toISOString(),
-                       title: dadosDoForm.get('title'),
-                       image: dadosDoForm.get('image'),
-                     }
-                     const comunidadesAtualizadas = [...comunidades, comunidade];
-                     setComunidades(comunidadesAtualizadas)
+                const comunidade = {
+                  id: new Date().toISOString(),
+                  title: dadosDoForm.get('title'),
+                  image: dadosDoForm.get('image'),
+                }
+                const comunidadesAtualizadas = [...comunidades, comunidade];
+                setComunidades(comunidadesAtualizadas)
+            }}>
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                  />
+              </div>
+              <div>
+                <input
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
 
-                  }} >
-                    <div>
-                      <input placeholder="Qual vai ser o nome da sua comunidade?" 
-                      name="title" 
-                      aria-label="Qual vai ser o nome da sua comunidade?"/>
-                    </div>
-
-                    <div>
-                      <input placeholder="Coloque uma URL para usarmos de capa" 
-                      name="image" 
-                      aria-label="Coloque uma URL para usarmos de capa"/>
-                    </div>
-
-                    <button>
-                        Criar comunidade
-                    </button>
-
-                  </form>                
-              </Box>            
-            </div>
-          
-          <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}> 
+              <button>
+                Criar comunidade
+              </button>
+            </form>
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBox title="Seguidores" items={seguidores} />
           <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
               Comunidades ({comunidades.length})
@@ -126,14 +158,13 @@ export default function Home() {
               })}
             </ul>
           </ProfileRelationsBoxWrapper>
-
-            <ProfileRelationsBoxWrapper>            
+          <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
               Pessoas da comunidade ({pessoasFavoritas.length})
             </h2>
 
             <ul>
-              {pessoasFavoritas.slice(0,6).map((itemAtual) => {
+              {pessoasFavoritas.map((itemAtual) => {
                 return (
                   <li key={itemAtual}>
                     <a href={`/users/${itemAtual}`}>
@@ -144,13 +175,8 @@ export default function Home() {
                 )
               })}
             </ul>
-
-            </ProfileRelationsBoxWrapper>
-         
-            <Box> 
-              Comunidades
-            </Box>
-          </div>          
+          </ProfileRelationsBoxWrapper>
+        </div>
       </MainGrid>
     </>
   )
